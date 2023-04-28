b8f341419cef7bf606fe99ad2ee1cb682c6b9ca1
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2023-04-28 17:01:07 +0200

    ssl cert warning

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {StatsChartComponent} from './stats-chart/stats-chart.component';
 import {HttpClientModule} from "@angular/common/http";
+import { WarningComponent } from './components/warning/warning.component';
+import {StatsChartComponent} from "./components/stats-chart/stats-chart.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatsChartComponent
+    StatsChartComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
